9d08c0b662bd6fa89a59efb2b4fd3d47bcaa22e8
sauravR5J <saurav.pradhan@ntellectdesign.com>
2018-07-06 12:16:46 +0530

    Practicing until Routing

---
+++ Using_Modules/module1.js
@@ -0,0 +1,8 @@
+function myFunction() {
+    console.log('Function1 was called');
+}
+
+var myString = 'String1';
+
+module.exports.myFunction = myFunction;
+module.exports.myString = myString;
---
+++ Using_Modules/module2.js
@@ -0,0 +1,7 @@
+module.exports = {
+    myFunction: function() {
+        console.log('Function2 was called');
+    },
+
+    myString: 'String2'
+};
