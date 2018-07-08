785238a3563811f3ff45fcf32dc872efb7ddab23
sauravR5J <pradhan.saurav133@gmail.com>
2018-07-08 22:08:25 +0530

    until validators and session

--- Express-Validators-Sessions/routes/index.js
+++ Express-Validators-Sessions/routes/index.js
@@ -3,16 +3,24 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express', condition: true, anyArray: [1,2,3] });
+  res.render('index', { title: 'Form Validation', success: req.session.success, errors: req.session.errors});
+  req.session.errors= null;
 });
 
-router.get('/test/:id', function(req, res, next) {
-  res.render('test', {output: req.params.id});
-});
+router.post('/submit', function(req, res, next) {
+  //check validaity
+  req.check('e-mail', 'Invalid email address').isEmail();
+  req.check('password', 'Password is Invalid').isLength({min: 4}).equals(req.body.confirmPassword);
 
-router.post('/test/submit', function(req, res, next) {
-  var id = req.body.id;
-  res.redirect('/test/' + id);
+  var errors = req.validationErrors();
+  if (errors) {
+    req.session.errors = errors;
+    req.session.success = false;
+  }
+  else {
+    req.session.success = true;
+  }
+  res.redirect('/');
 });
 
 module.exports = router;
