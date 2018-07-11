e4a09779d4eedee85d9ad2dcea9cc9611959f52c
sauravR5J <saurav.pradhan@ntellectdesign.com>
2018-07-11 17:16:36 +0530

    Started lerning mongodb connection

--- MongoDB(insert&get)/routes/index.js
+++ MongoDB(insert&get)/routes/index.js
@@ -3,9 +3,55 @@ var router = express.Router();
 var mongo = require('mongodb');
 var assert = require('assert');
 
+
+var url = 'mongodb://localhost:27017/test'
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express', condition: true, anyArray: [1,2,3] });
+});
+
+router.get('/get', function(req, res, next) {
+  var resultArray = [];
+  mongo.connect(url, function(err, db) {
+    assert.equal(null, err);
+    var cursor = db.collection('user-data').find();
+    cursor.forEach(function(doc, err) {
+      assert.equal(null, err);
+      resultArray.push(doc);
+    }, function() {
+      db.close();
+      res.render('index', {items: resultArray});
+    });
+  });
+});
+
+router.post('/insert', function(req, res, next) {
+  var item = {
+    title: req.body.title,
+    content: req.body.content,
+    author: req.body.author
+  };
+
+  mongo.connect(url, function(err, db) {
+    assert.equal(null, err);
+    console.log('connection done');
+    db.collection('user-data').insertOne(item, function(err, result) {
+      assert.equal(null, err);
+      console.log('Item inserted');
+      db.close();
+    });
+  });
+
+  res.redirect('/');
+});
+
+router.get('/update', function(req, res, next) {
+  
+});
+
+router.get('/delete', function(req, res, next) {
+  
 });
 
 
