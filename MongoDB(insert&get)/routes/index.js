a2e76034d025c9aa4bacdaac258c816bd652b470
sauravR5J <saurav.pradhan@ntellectdesign.com>
2018-07-12 14:50:22 +0530

    Delete and Update data mongodb node.js

--- MongoDB(insert&get)/routes/index.js
+++ MongoDB(insert&get)/routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
-var mongo = require('mongodb');
+var mongo = require('mongodb').MongoClient;
+var objectId = require('mongodb').ObjectID;
 var assert = require('assert');
 
 
@@ -11,7 +12,7 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express', condition: true, anyArray: [1,2,3] });
 });
 
-router.get('/get', function(req, res, next) {
+router.get('/get-data', function(req, res, next) {
   var resultArray = [];
   mongo.connect(url, function(err, db) {
     assert.equal(null, err);
@@ -35,23 +36,43 @@ router.post('/insert', function(req, res, next) {
 
   mongo.connect(url, function(err, db) {
     assert.equal(null, err);
-    console.log('connection done');
     db.collection('user-data').insertOne(item, function(err, result) {
       assert.equal(null, err);
       console.log('Item inserted');
       db.close();
     });
   });
-
-  res.redirect('/');
 });
 
-router.get('/update', function(req, res, next) {
-  
+router.post('/update', function(req, res, next) {
+  var item = {
+    title: req.body.title,
+    content: req.body.content,
+    author: req.body.author
+  };
+  var id = req.body.id;
+
+  mongo.connect(url, function(err, db) {
+    assert.equal(null, err);
+    db.collection('user-data').updateOne({"_id": objectId(id)}, {$set: item} , function(err, result) {
+      assert.equal(null, err);
+      console.log('Item updated');
+      db.close();
+    });
+  });
 });
 
-router.get('/delete', function(req, res, next) {
-  
+router.post('/delete', function(req, res, next) {
+  var id = req.body.id;
+
+  mongo.connect(url, function(err, db) {
+    assert.equal(null, err);
+    db.collection('user-data').deleteOne({"_id": objectId(id)}, function(err, result) {
+      assert.equal(null, err);
+      console.log('Item deleted');
+      db.close();
+    });
+  });
 });
 
 
